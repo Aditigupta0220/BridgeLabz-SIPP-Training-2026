7d501c767684740560a9376c7f36a0676b485737
Aditigupta0220 <aditi.gupta_cs23@gla.ac.in>
2026-06-17 09:23:05 +0530

    Add files via upload

---
+++ recursion/b.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    static int factorial(int n) {
+        // Base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // Recursive case
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
